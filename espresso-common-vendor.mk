bd6a85f68b8adbb4c9bae9bdc3fea91828a66e1d
tuxafgmur <tuxafgmur@users.noreply.github.com>
2019-02-12 12:08:50 +0100

    Removes widevine objects.
    These are found in the proprietary_vendor_widevine repo called from hardware_ti_omap4.

--- espresso-common-vendor.mk
+++ espresso-common-vendor.mk
@@ -13,10 +13,3 @@
 # limitations under the License.
 
 $(call inherit-product, vendor/samsung/espresso-common/common-vendor-blobs.mk)
-
-PRODUCT_PACKAGES += \
-    libdrmwvmplugin \
-    libwvdrmengine \
-    libwvdrm_L3 \
-    libwvm \
-    libWVStreamControlAPI_L3
